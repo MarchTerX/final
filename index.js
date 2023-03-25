e9fb508e366a9e27f3973db73cad5459024f2942
joemang01 <chanatip227@gmail.com>
2023-03-25 15:44:01 +0700

    backend v.4 by chanatip

--- index.js
+++ index.js
@@ -43,6 +43,35 @@ app.get('/search', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  let firstname = req.body.firstname
+  let lastname = req.body.lastname
+  let email = req.body.email
+  let username = req.body.username
+  let password = req.body.password
+  
+  let query = `INSERT INTO user (firstname, lastname, email, username, password) VALUES ('${firstname}', '${lastname}', '${email}', '${username}', '${password}')`
+  console.log(query)
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err 
+    res.send(rows)
+  })
+})
+
+app.get('/account', (req,res) => {
+    let username = req.query.username
+    let firstname = req.query.firstname
+    let lastname = req.query.lastname
+    let email = req.query.email
+
+    let query = `UPDATE user SET firstname = '${firstname}', lastname = '${lastname}', email = '${email}' WHERE username = '${username}'`
+    console.log(query)
+    connection.query(query, (err, rows, fields) => {
+        if(err) throw err
+        res.send(err)
+    })
+})
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
